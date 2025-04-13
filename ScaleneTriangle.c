2819197e5223e35c0a82a1995ba6198e843e0f32
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-04-13 16:21:25 +0530

    ScaleneTriangle.c

---
+++ ScaleneTriangle.c
@@ -0,0 +1,25 @@
+/*  Scalene Triangle
+Given A,B, and C as the sides of a triangle, find whether the triangle is scalene.
+
+Note:
+A triangle is said to be scalene if all three sides of the triangle are distinct.
+It is guaranteed that the sides represent a valid triangle.    */
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if(a!=b && b!=c && a!=c){
+                printf("YES\n");
+        }
+        else{
+                printf("NO\n");
+        }
+    }
+    return 0;
+}
