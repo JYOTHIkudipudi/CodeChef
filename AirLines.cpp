e2b8c2e083a40e175ee228760122a1d91dd65a3d
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-06-10 22:10:01 +0530

    AirLines.cpp

--- AirLines.cpp
+++ AirLines.cpp
@@ -1,7 +1,7 @@
 /*   Airlines
 An airline operates X aircraft every day. Each aircraft can carry up to 100 passengers.
 One day, N passengers would like to travel to the same destination. What is the minimum number of new planes that the airline must buy to carry all N passengers?
-
+ 
 Input Format
 The first line of input will contain a single integer T, denoting the number of test cases.
 Each test case consists of a single line containing two space-separated integers X and N 
