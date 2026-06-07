267be4c5ac768ff48269c7527852522c0e3c90c0
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-06-07 22:14:32 +0530

    Create FindinfProfession.cpp

---
+++ FindinfProfession.cpp
@@ -0,0 +1,33 @@
+/*  Finding Profession
+
+Consider a special family of Engineers and Doctors with following rules :
+
+Everybody has two children.
+First child of an Engineer is an Engineer and second child is a Doctor.
+First child of an Doctor is Doctor and second child is an Engineer.
+All generations of Doctors and Engineers start with Engineer.
+The first few levels of the family tree are shown below :
+
+
+Given level and position (pos) of a person in above ancestor tree, find profession of the person.
+
+Examples:
+
+Input: level = 4, pos = 2
+Output: Doctor
+Explaination: It is shown in the tree given in question.
+Input: level = 3, pos = 4
+Output: Engineer
+Explaination: Already given in the tree in question.
+Constraints:
+1 ≤ level ≤ 109
+1 ≤ pos ≤ 109   */
+
+class Solution {
+  public:
+    string profession(int level, int pos) {
+        int flips = __builtin_popcount(pos - 1);
+
+        return (flips % 2 == 0) ? "Engineer" : "Doctor";
+    }
+};
